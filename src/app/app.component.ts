0437c6831ab41e4fe145e55c05cf0c66eda3b3b7
Mohammed Amine Rochdi <mo.aminerochdi@gmail.com>
2025-06-25 21:34:45 +0200

    added files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingComponent } from "./features/landing/landing.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet, LandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './features/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent,
+    children: [
+      // { path: '', component:  },
+      // { path: '', component:  },
+      // etc.
+    ]
+  },
+  { path: '**', redirectTo: '' },
+];
